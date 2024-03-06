fa396e1d4882b72050f02fdf068af97ac59bc716
Juhan280 <92646057+Juhan280@users.noreply.github.com>
2024-03-06 22:35:33 +0600

    d01p1

---
+++ d01/p1.mjs
@@ -0,0 +1,37 @@
+import { createReadStream } from "node:fs";
+import { createInterface } from "node:readline";
+
+const stream = createReadStream("./data.txt", "utf8");
+const rl = createInterface(stream);
+
+/** @type {number[]} */
+const numbers = []
+
+rl.on("line", (line) => {
+  const { length } = line;
+  let first, last;
+
+  for (let i = 0; i < length; i++) {
+    const char = line[i];
+    if (isDigit(char)) {
+      first = char;
+      break;
+    }
+  }
+
+  for (let i = length - 1; i >= 0; i--) {
+    const char = line[i];
+    if (isDigit(char)) {
+      last = char;
+      break;
+    }
+  }
+
+  numbers.push(+(first + last))
+});
+
+rl.on("close", () => console.log(numbers.reduce((a, c) => a + c)))
+
+function isDigit(char) {
+  return char >= "0" && char <= "9";
+}
